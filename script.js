9f6d3676249b87adf19fcb7e5f973dcabd3060d9
neupanenischal <neupanenischal79@gmail.com>
2024-05-17 04:47:49 -0400

    pop up scroll
    animation added when the user scrolls pop up

--- script.js
+++ script.js
@@ -4,3 +4,18 @@ function toggleMenu() {
     menu.classList.toggle('open');
     icon.classList.toggle('open');
 }
+// scroll pop up Animation
+document.addEventListener("DOMContentLoaded", function () {
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("visible");
+                observer.unobserve(entry.target);
+            }
+        });
+    }, { threshold: 0.1 });
+
+    document.querySelectorAll("section").forEach(section => {
+        observer.observe(section);
+    });
+});
